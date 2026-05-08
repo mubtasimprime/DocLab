bfd3b0b9f1f262ae65fcbe676b3334a1f1993160
raiyan1999j <boomboomraiyan@gmail.com>
2026-05-08 10:03:48 +0600

    css steup

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -3,11 +3,7 @@ import { ReactNode } from "react";
 export default function DashLayout({children}:{children:ReactNode}){
     return(
         <>
-        <html>
-            <body>
-                {children}
-            </body>
-        </html>
+        
         </>
     )
 }
